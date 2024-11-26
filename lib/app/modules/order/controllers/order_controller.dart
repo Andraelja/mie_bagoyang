f613b0e717be5887a585163e0c1ec18bbe4c5dc7
Curve887 <kurniawanadeputra17@gmail.com>
2024-11-26 09:30:25 +0700

    fitur order & register

--- lib/app/modules/order/controllers/order_controller.dart
+++ lib/app/modules/order/controllers/order_controller.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 
-class LoginController extends GetxController {
-  //TODO: Implement LoginController
+class OrderController extends GetxController {
+  //TODO: Implement OrderController
 
   final count = 0.obs;
 
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,6 +4,8 @@ import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
+import '../modules/order/bindings/order_binding.dart';
+import '../modules/order/views/order_view.dart';
 import '../modules/register/bindings/register_binding.dart';
 import '../modules/register/views/register_view.dart';
 
@@ -12,7 +14,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.LOGIN ;
+  static const INITIAL = Routes.ORDER;
 
   static final routes = [
     GetPage(
@@ -29,6 +31,11 @@ class AppPages {
       name: _Paths.REGISTER,
       page: () => const RegisterView(),
       binding: RegisterBinding(),
+    ),
+    GetPage(
+      name: _Paths.ORDER,
+      page: () => const OrderView(),
+      binding: OrderBinding(),
     ),
   ];
 }
